e0b9af403db31ea65bb1294318e7247cdf525963
hafeez <hafeez.s@technovert.com>
2023-02-13 18:26:28 +0530

    Array of lists approach

--- ParkingLotSimulator/Data.cs
+++ ParkingLotSimulator/Data.cs
@@ -8,20 +8,20 @@ namespace ParkingLotSimulator
         public int Total2WheelerSlots { get; set; }
         public int Total4WheelerSlots { get; set; }
         public int TotalHeavyVehicleSlots { get; set; }
-        
+
         public int Occupied2WheelerSlots { get; set; }
         public int Occupied4WheelerSlots { get; set; }
         public int OccupiedHeavyVehicleSlots { get; set; }
 
         public List<Ticket>[] ParkingLot = new List<Ticket>[3];
         public List<Ticket>[] History = new List<Ticket>[3];
-        
+
         public Data()
-		{
+        {
             History[0] = new List<Ticket>();
             History[1] = new List<Ticket>();
             History[2] = new List<Ticket>();
         }
-	}
+    }
 }
 
