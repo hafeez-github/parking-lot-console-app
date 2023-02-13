b6d6a5eff7bb02c637e368b1be185af8cbc465f5
hafeez <hafeez.s@technovert.com>
2023-02-13 18:29:34 +0530

    Started new branch to implement dictionary approach

--- ParkingLotSimulator/Data.cs
+++ ParkingLotSimulator/Data.cs
@@ -5,17 +5,21 @@ namespace ParkingLotSimulator
 {
     public class Data
     {
-        public int Total2WheelerSlots { get; set; }
-        public int Total4WheelerSlots { get; set; }
-        public int TotalHeavyVehicleSlots { get; set; }
+        //public int Total2WheelerSlots { get; set; }
+        //public int Total4WheelerSlots { get; set; }
+        //public int TotalHeavyVehicleSlots { get; set; }
         
-        public int Occupied2WheelerSlots { get; set; }
-        public int Occupied4WheelerSlots { get; set; }
-        public int OccupiedHeavyVehicleSlots { get; set; }
+        //public int Occupied2WheelerSlots { get; set; }
+        //public int Occupied4WheelerSlots { get; set; }
+        //public int OccupiedHeavyVehicleSlots { get; set; }
 
-        public List<Ticket>[] ParkingLot = new List<Ticket>[3];
+        //public List<Ticket>[] ParkingLot = new List<Ticket>[3];
         public List<Ticket>[] History = new List<Ticket>[3];
-        
+
+        public List<Ticket> CurrentTicketList = new List<Ticket>();
+        public List<Ticket> HistoricalTicketList = new List<Ticket>();
+        public Dictionary<string, int> ParkingLot = new Dictionary<string, int>();
+
         public Data()
 		{
             History[0] = new List<Ticket>();
